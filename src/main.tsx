ba90b043f09171a5362f4fef22b7f7b574529939
Alena Alioshyna <elenakovalski1995@yandex.ru>
2025-09-18 12:52:07 +0300

    2 week

--- src/main.tsx
+++ src/main.tsx
@@ -322,3 +322,236 @@
 // createRoot(document.getElementById("root")!).render(<User name={'John'} description={'lorem ipsum'} />)
 
 // // Что нужно написать вместо '❗X', что бы в браузере отобразилось описание, которое передали в пропсах ?
+
+
+
+
+
+// const user = { }
+// const student = { }
+
+// user !==  student 
+
+// /*Каков результат выполнения этого выражения?*/
+
+
+
+// ❓НЕ РЕШИЛА ПРАВИЛЬНО!
+// import { useState } from "react"
+// import { createRoot } from "react-dom/client"
+
+// function UsersList() {
+//   const [users, setUsers] = useState<string[]>(["Bob", "Alex", "Ann"])
+//   const getUser = (user: string) => <li>{user}</li>
+
+//   return (
+//     <div>
+//       <h4>User list:</h4>
+//       <ul>{users.map(getUser)}</ul>
+//     </div>
+//   )
+// }
+
+// createRoot(document.getElementById("root")!).render(<UsersList />)
+
+// // Что вернёт выражение: typeof getUser?
+
+
+
+//❓НЕ РЕШИЛА ПРАВИЛЬНО!
+// import { useState } from "react"
+// import { createRoot } from "react-dom/client"
+
+// function UsersList() {
+//   const results = useState<string[]>(["Bob", "Alex", "Ann"])
+
+//   const users = results[0]
+//   const setUsers = results[1]
+
+//   return <p>Тут будет список пользователей</p>
+// }
+
+// createRoot(document.getElementById("root")!).render(<UsersList />)
+
+// // Чему равно results.length? 3
+
+
+
+
+// import { useState } from "react"
+// import { createRoot } from "react-dom/client"
+
+// function UsersList() {
+//   const [users, setUsers] = useState<string[]>(["Bob", "Alex", "Ann"])
+//   const getUser = (user: string) => <li>{user}</li>
+//   const usersList = users.map(getUser)
+
+//   return (
+//     <div>
+//       <h4>User list:</h4>
+//       <ul>{usersList}</ul>
+//     </div>
+//   )
+// }
+
+// createRoot(document.getElementById("root")!).render(<UsersList />)
+
+// // Что вернёт выражение: Array.isArray(usersList)?
+
+
+
+// import { useState } from "react"
+// import { createRoot } from "react-dom/client"
+
+// function UsersList() {
+//   const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
+//   const getUser = (user: string) => <li>{user}</li>
+//   const usersList = users.map(getUser)
+
+//   return (
+//     <div>
+//       <h4>User list:</h4>
+//       <ul>{usersList}</ul>
+//     </div>
+//   )
+// }
+
+// createRoot(document.getElementById("root")!).render(<UsersList />)
+
+// // Какую переменную необходимо указать вместо XXX, чтобы код заработал?
+
+
+
+
+
+// import { useState } from "react"
+// import { createRoot } from "react-dom/client"
+
+// type User = {
+//   id: number
+//   name: string
+//   age: number
+// }
+
+// const UsersList = () => {
+//   const state = [
+//     { id: 1, name: "Bob", age: 34 },
+//     { id: 2, name: "Alex", age: 25 },
+//     { id: 3, name: "Ann", age: 30 },
+//     { id: 4, name: "John", age: 23 },
+//   ]
+
+//   const users = [
+//     { id: 1, userName: "Bob", age: 34 },
+//     { id: 2, userName: "Alex", age: 25 },
+//     { id: 3, userName: "Ann", age: 30 },
+//     { id: 4, userName: "John", age: 23 },
+//   ]
+
+//   const [usersList, setUsersList] = useState<User[]>(state)
+
+//   return (
+//     <>
+//       <h2>User list:</h2>
+//       <p>Тут будет список пользователей</p>
+//     </>
+//   )
+// }
+
+// createRoot(document.getElementById("root")!).render(<UsersList />)
+
+// // Что надо написать вместо XXX, чтобы код работал?
+// // ❗ Если мы отмапим массив, то должны увидеть данные пользователей
+// // ❗ Ответ дать минимально возможным объёмом кода
+
+
+
+
+
+
+// import { useState } from "react"
+// import { createRoot } from "react-dom/client"
+
+// function UsersList() {
+//   const results = useState<string[]>(["Bob", "Alex", "Ann"])
+//   const users = results[0]
+//   const setUsers = results[1]
+
+//   return <p>Тут будет список пользователей</p>
+// }
+
+// createRoot(document.getElementById("root")!).render(<UsersList />)
+
+// // Какой тип правильнее указать вместо "any" при типизации стэйта?
+
+
+
+
+// import { useState } from "react"
+// import { createRoot } from "react-dom/client"
+
+// function UsersList() {
+//   const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
+
+//   return <p>Тут будет список пользователей!</p>
+// }
+
+// createRoot(document.getElementById("root")!).render(<UsersList />)
+
+// // Что вернёт выражение: typeof useState? function
+
+
+
+
+// import { createRoot } from "react-dom/client"
+// import { useState } from "react"
+
+// function UsersList() {
+//   const [users, setUsers] = useState<string[]>(["Bob", "Alex", "Ann"])
+
+//   return <p>Тут будет список пользователей</p>
+// }
+
+// createRoot(document.getElementById("root")!).render(<UsersList />)
+
+// // Что вернёт выражение: Array.isArray(users)?
+
+
+
+
+// import { createRoot } from "react-dom/client"
+// import { useState } from "react"
+
+// function UsersList() {
+//   const [users, setUsers] = useState<string[]>(["Bob", "Alex", "Ann"])
+
+//   return (
+//     <div>
+//       <h4>User list:</h4>
+//       <ul>
+//         {users.map((user) => (
+//           <li>{user}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   )
+// }
+
+// createRoot(document.getElementById("root")!).render(<UsersList />)
+
+// // Что надо вставить вместо ХХХ, чтобы код заработал?
+
+
+
+
+// import { createRoot } from "react-dom/client"
+// import { useState } from "react"
+
+// function UsersList() {
+//   const [users, setUsers] = useState<Array<string>>(["Bob", "Alex", "Ann"])
+//   return <p>Тут будет список пользователей</p>
+// }
+
+// createRoot(document.getElementById("root")!).render(<UsersList />)
+
+// // Что вернёт выражение: typeof setUsers? function
